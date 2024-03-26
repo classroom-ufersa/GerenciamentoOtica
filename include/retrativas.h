ce60274ac0439653dec2d0b6f25129a16067d468
biiaalmeida <abeatrizpb@gmail.com>
2024-03-25 23:44:36 -0300

    feat: added function declarations to retrativas.h

---
+++ include/retrativas.h
@@ -0,0 +1,9 @@
+int numero_inteiroc(char *str);
+
+void formata_string(char *str);
+
+int verifica_codigo(char *id_digitado);
+
+int contem_apenas_letras(char *str);
+
+void formata_string(char *str);
