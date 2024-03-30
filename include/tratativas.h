5ea2c846ae9dcd3c6073aa67e0c93f22ffd744ac
hillaryds <hillary.saldanha199@gmail.com>
2024-03-30 20:14:38 -0300

    feat: added cabecalho function.

---
+++ include/tratativas.h
@@ -0,0 +1,24 @@
+
+#ifndef TRATATIVAS_H
+#define TRATATIVAS_H
+
+// Path: GerenciamentoOtica/include/tratativas.h
+#include "../include/consultas.h"
+
+void cria_lista_consultas(ListaConsultas **lista);
+
+int numero_inteiroc(char *str);
+
+int contem_apenas_letras(char *str);
+
+long long data_para_num(char *data);
+
+char *num_para_data(long long data);
+
+int data_valida(char *data);
+
+char *formata_cpf(char *cpf);
+
+void cabecalho(char* linha, char* titulo);
+
+#endif
