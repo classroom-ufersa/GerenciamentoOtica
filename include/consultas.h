4279ea009822fb4078696fac673b84e980439e57
hillaryds <hillary.saldanha199@gmail.com>
2024-04-01 14:22:07 -0300

    refactor: functions added.

--- include/consultas.h
+++ include/consultas.h
@@ -1,20 +1,12 @@
 #ifndef CONSULTAS_H
 #define CONSULTAS_H
 
+#include "../src/tratativas.c"
+
 typedef struct consulta Consulta;
 
-typedef struct lista_consultas ListaConsultas;
+Consulta *adiciona_consulta(Consulta * lista_consultas, Consulta nova_consulta);
 
-int verifica_cpf(ListaConsultas **lista, char cpf[20]);
-
-int verifica_id_consulta(ListaConsultas **lista, char id[10]);
-
-void adicionar_consulta(ListaConsultas **lista);
-
-void remover_consulta_por_id(ListaConsultas **lista, char id[10]);
-
-void imprimir_consultas (ListaConsultas **lista);
-
-void adicionar_no_arquivo(ListaConsultas **lista);
+Consulta nova_consulta(int *qnt);
 
 #endif
