c865cf5ec620f81c8cfe0a4630ff12d811ac3e08
hillaryds <hillary.saldanha199@gmail.com>
2024-04-03 14:56:14 -0300

    refactor: added verifica_cpf_paciente_edicao, editar_paciente_lista_paciente, edita_paciente functions

--- include/paciente.h
+++ include/paciente.h
@@ -25,7 +25,13 @@ Paciente * encontra_paciente(Paciente * lista_pacientes, char cpf[12]);
 
 int verifica_cpf_paciente(Paciente *lista_pacientes, char cpf[15]);
 
+int verifica_cpf_paciente_edicao(Paciente *lista_pacientes, char cpf_editado[20], char cpf_antigo[20]);
+
 void lista_paciente(Paciente *lista_pacientes);
+
+Paciente * editar_paciente_lista_paciente(Paciente *lista_pacientes);
+
+Paciente * edita_paciente(Paciente * lista_pacientes, char cpf_antigo[20]);
 
 void escreve_no_arquivo(Paciente *lista_para_pacientes, char *local_do_arquivo);
 
