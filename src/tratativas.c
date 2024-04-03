dd62a0f15b75135aeea5b7a6d9f8a2e0756d0c9a
biiaalmeida <abeatrizpb@gmail.com>
2024-04-03 18:52:49 -0300

    feat: added functions

--- src/tratativas.c
+++ src/tratativas.c
@@ -19,6 +19,59 @@ int contem_apenas_letras(char *str) {
     return 1;
 }
 
+long long data_para_num(char *data) {
+    int dia, mes;
+    long long ano;
+    long long tempo_dia = 0;
+
+    // quantidade de dias de cada mês:
+    //               J   F   M   A   M   J   J   A   S   O   N   D 
+    int meses[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    
+    sscanf(data, "%d/%d/%lld", &dia, &mes, &ano);     /* separa os valores da data */
+
+    tempo_dia += (ano-1) * 365;     /* converte ano em dias */
+    
+
+    /* Conversão de meses em dias */
+    int i;
+    for (i = 0; i < (mes-1); i++)
+    {
+        tempo_dia += meses[i];      /* incrementa os dias de cada mês completo */
+    }
+    
+    tempo_dia += (dia-1);       /* soma os dias no resultado do cálculo */
+
+    return tempo_dia;
+}
+
+char *num_para_data(long long data) { 
+    long long ano = 0; 
+    int mes = 0, dia = 0;
+    int dia_mes;
+
+    // quantidade de dias de cada mês:
+    //               J   F   M   A   M   J   J   A   S   O   N   D 
+    int meses[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    ano = (data / 365);
+    
+    /* conversão de dias em meses */
+    dia_mes = (data % 365);
+    while (dia_mes >= meses[mes])    /* verifica os meses, de janeiro a dezembro */
+    {
+        dia_mes -= meses[mes];      /* decrementa a quantidade de dias do mês */
+        mes++;                      /* contabiliza os meses completos */
+    }
+
+    dia = dia_mes;                  /* o resto do cálculo será o dia */
+
+    char *data_format = (char*)malloc(11*sizeof(char));
+    sprintf(data_format, "%02d/%02d/%04lld", dia+1, mes+1, ano+1); 
+
+    return data_format;
+}
+
 int data_valida(char *data) {
     int dia, mes, ano;
     sscanf(data, "%d/%d/%d", &dia, &mes, &ano);
